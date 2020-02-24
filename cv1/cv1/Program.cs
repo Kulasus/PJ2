3b47fec0a6100b07add4d53c3d362ea74c3dba89
Lukáš Kondiolka <lukaskondiolka@seznam.cz>
2020-02-24 21:01:44 +0100

    Added cv1

---
+++ cv1/cv1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using cv1lib;
+
+namespace cv1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String myName = "Lukas";
+            Greeter greeter = new Greeter();
+            greeter.greet(myName);
+        }
+    }
+}
---
+++ cv1/cv1lib/Greeter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cv1lib
+{
+    public class Greeter
+    {
+        public void greet(String name)
+        {
+            Console.WriteLine("Hello " + name + "!");
+        }
+    }
+}
